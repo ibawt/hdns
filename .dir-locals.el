c70e1b36c3cf15c6b602fd7857b2633826086ed6
Ian Quick <ian.quick@gmail.com>
2017-09-02 15:03:00 -0400

    some tests

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "hdns:lib" "hdns:exe:hdns-exe" "hdns:test:hdns-test")))
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,6 @@
 module Lib
   ( readPacket
-  , writeMessage
-  , readMessage
-  , fromFile
-  , test
+  , writePacket
   ) where
 
 import           Control.Concurrent
@@ -26,29 +23,33 @@ import           Network.Socket
 import qualified Network.Socket.ByteString   as S
 import           System.IO
 
-data Message = Message{ txId     :: Word16
-                      , flags     :: Word16
-                      , questions :: [Question]
-                      , answers   :: [ResourceRecord]
-                      } deriving (Show, Eq)
+data Message = Message
+  { txId :: Word16
+  , flags :: Word16
+  , questions :: [Question]
+  , answers :: [ResourceRecord]
+  } deriving (Show, Eq)
 
-data Question = Question { name   :: [B.ByteString]
-                         , qtype  :: Word16
-                         , qclass :: Word16
-                         } deriving (Show, Eq)
+data Question = Question
+  { name :: [B.ByteString]
+  , qtype :: Word16
+  , qclass :: Word16
+  } deriving (Show, Eq)
 
-data ResourceRecord = ResourceRecord { rname  :: [B.ByteString]
-                                     , rtype  :: Word16
-                                     , rclass :: Word16
-                                     , rttl   :: Int32
-                                     , rdata  :: ResourceData
-                                     } deriving (Show, Eq)
+data ResourceRecord = ResourceRecord
+  { rname :: [B.ByteString]
+  , rtype :: Word16
+  , rclass :: Word16
+  , rttl :: Int32
+  , rdata :: ResourceData
+  } deriving (Show, Eq)
 
 type ResourceRecordCache = HM.HashMap [B.ByteString] [ResourceRecord]
 
-data ResourceData =
-    IPAddr (Word8, Word8, Word8, Word8)
-  | Bytes B.ByteString deriving (Show, Eq)
+data ResourceData
+  = IPAddr (Word8, Word8, Word8, Word8)
+  | Bytes B.ByteString
+  deriving (Show, Eq)
 
 type LabelMap = HM.HashMap [B.ByteString] Int
 
@@ -59,7 +60,7 @@ writeLabels labels = do
     Nothing -> do
       mapM_ Lib.putByteString labels
       lift $ putWord8 0
-      put (HM.insert labels pos lm, pos + 1 )
+      put (HM.insert labels pos lm, pos + 1)
     Just pos -> putWord16 $ 0xc000 .|. fromIntegral pos
 
 incPos :: Int -> StateT (LabelMap, Int) PutM()
@@ -90,31 +91,28 @@ writeMessage msg = do
   putWord16 $ fromIntegral $ length (answers msg)
   putWord16 0
   putWord16 0
-
-  mapM_ (\q -> do
-           writeLabels $ name q
-           putWord16 $ qtype q
-           putWord16 $ qclass q
-       ) $ questions msg
-
-
-  mapM_ (\r -> do
-           writeLabels (rname r)
-           putWord16 $ rtype r
-           putWord16 $ rclass r
-           putInt32 $ rttl r
-
-           case rdata r of
-             IPAddr (a, b, c, d) -> do
-               putWord16 4
-               lift $ putWord8 a
-               lift $ putWord8 b
-               lift $ putWord8 c
-               lift $ putWord8 d
-               incPos 4
-             Bytes x ->
-               Lib.putByteString x
-       ) $ answers msg
+  mapM_
+    (\q -> do
+       writeLabels $ name q
+       putWord16 $ qtype q
+       putWord16 $ qclass q) $
+    questions msg
+  mapM_
+    (\r -> do
+       writeLabels (rname r)
+       putWord16 $ rtype r
+       putWord16 $ rclass r
+       putInt32 $ rttl r
+       case rdata r of
+         IPAddr (a, b, c, d) -> do
+           putWord16 4
+           lift $ putWord8 a
+           lift $ putWord8 b
+           lift $ putWord8 c
+           lift $ putWord8 d
+           incPos 4
+         Bytes x -> Lib.putByteString x) $
+    answers msg
 
 
 readLabel = do
@@ -153,13 +151,12 @@ readAnswer bytes = do
   rclass <- getWord16be
   ttl <- fromIntegral <$> getWord32be
   rd_len <- getWord16be
-
-  rdata <- case rtype of
-            1 -> readIP
-            _ -> do
-               b <- getByteString $ fromIntegral rd_len
-               return $ Bytes b
-
+  rdata <-
+    case rtype of
+      1 -> readIP
+      _ -> do
+        b <- getByteString $ fromIntegral rd_len
+        return $ Bytes b
   return $ ResourceRecord name rtype rclass ttl rdata
 
 
@@ -176,24 +173,22 @@ readMessage bytes = do
   an_count <- getWord16be
   ns_count <- getWord16be
   ar_count <- getWord16be
-
-  queries <- mapM (\_ -> readQuestion bytes) [1..query_count]
-  answers <- mapM (\_ -> readAnswer bytes) [1..an_count]
-
+  queries <- mapM (\_ -> readQuestion bytes) [1 .. query_count]
+  answers <- mapM (\_ -> readAnswer bytes) [1 .. an_count]
   return $ Message txId flags queries answers
 
 readPacket :: B.ByteString -> IO Message
 readPacket bytes = do
-  let r =  runGet (readMessage bytes) bytes
+  let r = runGet (readMessage bytes) bytes
   case fst r of
     Left error -> fail error
-    Right x    -> return x
+    Right x -> return x
 
-writePacket :: Message -> IO BL.ByteString
+writePacket :: Message -> IO B.ByteString
 writePacket m = do
   let r = evalStateT (writeMessage m) (HM.empty, 0)
   let x = runPut r
-  return x
+  return $ BL.toStrict x
 
 fromFile s = do
   b <- B.readFile s
@@ -202,14 +197,14 @@ fromFile s = do
 test s = do
   p <- fromFile s
   b <- writePacket p
-  hexDump (BL.toStrict b)
-  readPacket (BL.toStrict b)
+  hexDump b
+  readPacket b
 
 readBuffer bytes =
-  let (x, _) = runGet (readMessage bytes) bytes in
-    case x of
-      Left error -> fail error
-      Right x    -> return x
+  let (x, _) = runGet (readMessage bytes) bytes
+  in case x of
+       Left error -> fail error
+       Right x -> return x
 
 forwardRequest :: B.ByteString -> IO (Maybe Message)
 forwardRequest = undefined
